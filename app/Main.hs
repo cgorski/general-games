c3f54379e4815564dc89c7a40a9471eeffb2a984
Christopher A. Gorski <cgorski@cgorski.org>
2017-02-22 07:43:38 +0000

    Refactor typeclasses

--- app/Main.hs
+++ app/Main.hs
@@ -1,36 +1,39 @@
 module Main where
 
 import Game.Implement.Card.Standard
-import Game.Game.Poker
+import Game.Implement.Card
+-- --import Game.Game.Poker
 
 main :: IO ()
-main =
-  let r = Ace
-      s = Clubs
-      cards = mkCard r s
-      min = minBound :: Rank
-      max = maxBound :: Rank
-      l = cardLst
-      n = numCards in
-    do
---      putStrLn $ show cards
---      putStrLn $ show min
---      putStrLn $ show max
---      putStrLn $ show l
---      putStrLn $ show n
---      putStrLn $ show $ take 100 allPossibleHands
---      putStrLn $ show (length allPossibleHands)
---      putStrLn $ show (length allPossibleHands)
---      putStrLn $ show (length allPossibleHands)
---      putStrLn $ show (length allPossibleHands)
---      putStrLn $ show allRoyalFlush
---      putStrLn $ show (length allRoyalFlush)
-      putStrLn $ show (length allStraightFlush)
-      putStrLn $ show allStraightFlush
+main = do
+  putStrLn (show $ ValuePair 1 'w')
 
---      putStrLn $ show (take 10 allStraight)
---      putStrLn $ show allStraight
---      putStrLn $ show (length allStraight)
---      putStrLn $ show (length allFourOfAKind)
+--   let r = Ace
+--       s = Clubs
+--       cards = mkCard r s
+--       min = minBound :: Rank
+--       max = maxBound :: Rank
+--       l = cardLst
+--       n = numCards in
+--     do
+-- --      putStrLn $ show cards
+-- --      putStrLn $ show min
+-- --      putStrLn $ show max
+-- --      putStrLn $ show l
+-- --      putStrLn $ show n
+-- --      putStrLn $ show $ take 100 allPossibleHands
+-- --      putStrLn $ show (length allPossibleHands)
+-- --      putStrLn $ show (length allPossibleHands)
+-- --      putStrLn $ show (length allPossibleHands)
+-- --      putStrLn $ show (length allPossibleHands)
+-- --      putStrLn $ show allRoyalFlush
+-- --      putStrLn $ show (length allRoyalFlush)
+--       putStrLn $ show (length allStraightFlush)
+--       putStrLn $ show allStraightFlush
+
+-- --      putStrLn $ show (take 10 allStraight)
+-- --      putStrLn $ show allStraight
+-- --      putStrLn $ show (length allStraight)
+-- --      putStrLn $ show (length allFourOfAKind)
       
   
